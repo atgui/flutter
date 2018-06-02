50da18739daebab69f355bb940ad4efe2b1f3465
agui <mhgui021@gmail.com>
2018-06-02 16:01:28 +0700

    a

---
+++ ftest1/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,16 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+#import <device_info/DeviceInfoPlugin.h>
+#import <video_player/VideoPlayerPlugin.h>
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FLTDeviceInfoPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTDeviceInfoPlugin"]];
+  [FLTVideoPlayerPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTVideoPlayerPlugin"]];
+}
+
+@end
